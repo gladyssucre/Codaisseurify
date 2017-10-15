187397bb3793c4d6f7a3864b80830b46a5276721
Gladys Sucre <gladyssucre@GLADYS.local>
2017-10-15 14:24:54 +0200

    no save yet

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -8,15 +8,36 @@ class SongsController < ApplicationController
    end
 
    def new
-     @song= Song.new
+      #@song= current_artist.songs.build
+    @song= Song.new
    end
 
    def create
-     @song = Song.new
+#@booking = current_user.bookings.new(booking_params.merge(event_id: params[:event_id]))
+    @song = Song.new(song_params)
+#variable.atribute= "value"
+    #current_artist.songs.new
      if @song.save
-       redirect_to @artist, notice: "Song saved"
+       redirect_to @artist, notice: "Event created"
      else
        render :new
      end
    end
+
+
+
+   private
+
+  def current_artist
+    artist_id=@artist
+  end
+
+  def song_params
+    params
+    .require(:song)
+    .permit(:name, :album, :year, :lyrics, :artist)
+  end
+
+#  t.bigint "artist_id"
+#  t.index ["artist_id"], name: "index_songs_on_artist_id"
 end
